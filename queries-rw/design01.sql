14263e7c052ffd6ce52fb7bf43d2ef3b3f3c947a
hichuang <mhi.chuang@gmail.com>
2012-12-08 18:13:35 -0500

    query1 mv

--- queries-rw/design01.sql
+++ queries-rw/design01.sql
@@ -1,20 +1,18 @@
 -- QUERY 1
-/*drop table if exists mv_q01;
+drop table if exists mv_q01;
 create table mv_q01
-	select l_returnflag,
-		   l_linestatus,
-		   l_shipdate,
-		   sum(l_quantity) as sum_qty
-		   sum(l_extendedprice) as sum_base_price
-		   sum(l_extendedprice * (1-l_discount)) as sum_disc_price,
-		   sum(l_extendedprice * (1-l_discount) * (1-l_tax)) as sum_charge,
-		   sum(l_discount) as sum_disc,
-		   count(*) as count_order
+	select mv_returnflag,
+		   mv_linestatus,
+		   mv_shipdate,
+		   sum(l_quantity) as mv_sum_qty
+		   sum(l_extendedprice) as mv_sum_base_price
+		   sum(l_extendedprice * (1-l_discount)) as mv_sum_disc_price,
+		   sum(l_extendedprice * (1-l_discount) * (1-l_tax)) as mv_sum_charge,
+		   sum(l_discount) as mv_sum_disc,
+		   count(*) as mv_count_order
 	from
 		lineitem
 	group by
 		l_returnflag,
 		l_linestatus,
-		l_shipdate;*/
-DROP INDEX shipdate ON lineitem;
-CREATE INDEX shipdate ON lineitem( l_shipdate );
+		l_shipdate;
--- queries-rw/q01.sql
+++ queries-rw/q01.sql
@@ -1,22 +1,22 @@
 -- QUERY 1
 select
-	l_returnflag,
-	l_linestatus,
-	sum(l_quantity) as sum_qty,
-	sum(l_extendedprice) as sum_base_price,
-	sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,
-	sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,
-	avg(l_quantity) as avg_qty,
-	avg(l_extendedprice) as avg_price,
-	avg(l_discount) as avg_disc,
-	count(*) as count_order
+	mv_returnflag,
+	mv_linestatus,
+	sum(mv_sum_qty) as sum_qty,
+	sum(mv_sum_base_price) as sum_base_price,
+	sum(mv_sum_disc_price) as sum_disc_price,
+	sum(mv_sum_charge) as sum_charge,
+	sum(mv_sum_qty) / sum(mv_count_order) as avg_qty,
+	sum(mv_sum_base_price) / sum(mv_count_order) as avg_price,
+	sum(mv_sum_disc) / sum(mv_count_order) as avg_disc,
+	sum(mv_count_order) as count_order
 from
-	lineitem
+	mv_q01
 where
-	l_shipdate <= date '1998-12-01'
+	mv_shipdate <= date '1998-12-01'
 group by
-	l_returnflag,
-	l_linestatus
+	mv_returnflag,
+	mv_linestatus
 order by
-	l_returnflag,
-	l_linestatus;
+	mv_returnflag,
+	mv_linestatus;

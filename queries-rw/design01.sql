be9fd0a730e92c6dbc7f38e2cd88a7e6b27ba742
hichuang <mhi.chuang@gmail.com>
2012-12-08 18:15:44 -0500

    query1 mv

--- queries-rw/design01.sql
+++ queries-rw/design01.sql
@@ -4,8 +4,8 @@ create table mv_q01
 	select mv_returnflag,
 		   mv_linestatus,
 		   mv_shipdate,
-		   sum(l_quantity) as mv_sum_qty
-		   sum(l_extendedprice) as mv_sum_base_price
+		   sum(l_quantity) as mv_sum_qty,
+		   sum(l_extendedprice) as mv_sum_base_price,
 		   sum(l_extendedprice * (1-l_discount)) as mv_sum_disc_price,
 		   sum(l_extendedprice * (1-l_discount) * (1-l_tax)) as mv_sum_charge,
 		   sum(l_discount) as mv_sum_disc,

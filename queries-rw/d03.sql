78f751bde651b1f04ed45f90a23d3bb739c6efa6
hichuang <mhi.chuang@gmail.com>
2012-12-08 21:47:27 -0500

    query3 mv

--- queries-rw/d03.sql
+++ queries-rw/d03.sql
@@ -5,7 +5,9 @@ create table mv_q03
 		l_orderkey as mv_orderkey,
 		sum(l_extendedprice * (1 - l_discount)) as mv_revenue,
 		o_orderdate as mv_orderate,
-		o_shippriority as mv_shippriority
+		o_shippriority as mv_shippriority,
+		o_shipdate as mv_shipdate,
+		c_mktsegment as mv_mktsegment
 	from
 		customer,
 		orders,

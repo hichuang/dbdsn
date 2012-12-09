95f8e10fb4e50c3ee8436db6b707884cc4cd5134
hichuang <mhi.chuang@gmail.com>
2012-12-08 21:22:37 -0500

    query3 mv

---
+++ queries-rw/d03.sql
@@ -0,0 +1,17 @@
+-- QUERY 3
+drop table if exists mv_q03;
+create table mv_q03
+	select
+		l_orderkey,
+		l_extendedprice * (1 - l_discount) as mv_revenue,
+		o_orderdate,
+		o_shippriority,
+		l_shipdate,
+		c_mktsegment,
+	from
+		customer,
+		orders,
+		lineitem
+	where
+		c_custkey = o_custkey
+		and l_orderkey = o_orderkey
--- queries-rw/q03.sql
+++ queries-rw/q03.sql
@@ -1,17 +1,13 @@
 -- QUERY 3
 select
 	l_orderkey,
-	sum(l_extendedprice * (1 - l_discount)) as revenue,
+	sum(mv_revenue) as revenue,
 	o_orderdate,
 	o_shippriority
 from
-	customer,
-	orders,
-	lineitem
+	mv_q03
 where
 	c_mktsegment = 'BUILDING'
-	and c_custkey = o_custkey
-	and l_orderkey = o_orderkey
 	and o_orderdate < date '1995-03-15'
 	and l_shipdate > date '1995-03-15'
 group by

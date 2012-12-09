511056f18886962237c11e2363c9ee786971c40d
hichuang <mhi.chuang@gmail.com>
2012-12-08 21:42:58 -0500

    query3 mv

--- queries-rw/d03.sql
+++ queries-rw/d03.sql
@@ -2,16 +2,24 @@
 drop table if exists mv_q03;
 create table mv_q03
 	select
-		l_orderkey,
-		l_extendedprice * (1 - l_discount) as mv_revenue,
-		o_orderdate,
-		o_shippriority,
-		l_shipdate,
-		c_mktsegment
+		l_orderkey as mv_orderkey,
+		sum(l_extendedprice * (1 - l_discount)) as mv_revenue,
+		o_orderdate as mv_orderate,
+		o_shippriority as mv_shippriority
 	from
 		customer,
 		orders,
 		lineitem
 	where
 		c_custkey = o_custkey
-		and l_orderkey = o_orderkey;
+		and l_orderkey = o_orderkey
+	group by
+		l_orderkey,
+		o_orderdate,
+		o_shippriority,
+		o_orderdate,
+		l_shipdate,
+		c_mktsegment
+	order by
+		revenue desc,
+		o_orderdate;

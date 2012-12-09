1b7101251120cba051b9b70bb84a383858bcd549
hichuang <mhi.chuang@gmail.com>
2012-12-08 19:46:50 -0500

    query2 hash index

--- queries-rw/d02.sql
+++ queries-rw/d02.sql
@@ -1,3 +1,3 @@
 -- QUERY 2
 create index part_size using HASH on part ( p_size );
-create index region_name on using HASH on region ( r_name );
+create index region_name using HASH on region ( r_name );

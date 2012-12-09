19e5978805f15694c06852fe19e6ead2d5865e47
hichuang <mhi.chuang@gmail.com>
2012-12-09 17:24:27 -0500

    query5 mv

--- queries-rw/d05.sql
+++ queries-rw/d05.sql
@@ -2,26 +2,28 @@
 drop table if exists mv_q05;
 create table mv_q05
 	select
-		n_name,
-	sum(l_extendedprice * (1 - l_discount)) as revenue
-from
-	customer,
-	orders,
-	lineitem,
-	supplier,
-	nation,
-	region
-where
-	c_custkey = o_custkey
-	and l_orderkey = o_orderkey
-	and l_suppkey = s_suppkey
-	and c_nationkey = s_nationkey
-	and s_nationkey = n_nationkey
-	and n_regionkey = r_regionkey
-	and r_name = 'ASIA'
-	and o_orderdate >= date '1994-01-01'
-	and o_orderdate < date '1994-01-01' + interval '1' year
-group by
-	n_name
-order by
-	revenue desc;
+		n_name as mv_nname,
+		o_orderdate as mv_orderdate,
+		r_name as mv_rname,
+		sum(l_extendedprice * (1 - l_discount)) as mv_revenue
+	from
+		customer,
+		orders,
+		lineitem,
+		supplier,
+		nation,
+		region
+	where
+		c_custkey = o_custkey
+		and l_orderkey = o_orderkey
+		and l_suppkey = s_suppkey
+		and c_nationkey = s_nationkey
+		and s_nationkey = n_nationkey
+		and n_regionkey = r_regionkey
+	group by
+		r_name,
+		o_orderdate,
+		n_name
+	order by
+		mv_rname,
+		mv_orderdate;

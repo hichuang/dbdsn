4b06b7bcd723be26280a9d8f1b7fc289963ebe9a
hichuang <mhi.chuang@gmail.com>
2012-12-10 03:27:21 -0500

    query10 mv

---
+++ queries-rw/d10.sql
@@ -0,0 +1,33 @@
+-- QUERY 10
+drop table if exists mv_q10;
+create table mv_q10
+	select
+		c_custkey as mv_custkey,
+		c_name as mv_cname,
+		sum(l_extendedprice * (1 - l_discount)) as mv_revenue,
+		c_acctbal as mv_acctbal,
+		n_name as mv_nname,
+		c_address as mv_addr,
+		c_phone as mv_phone,
+		c_comment as mv_comment,
+		o_orderdate as mv_orderdate,
+		l_returnflag as mv_returnflag
+	from
+		customer,
+		orders,
+		lineitem,
+		nation
+	where
+		c_custkey = o_custkey
+		and l_orderkey = o_orderkey
+		and c_nationkey = n_nationkey
+	group by
+		l_returnflag,
+		o_orderdate,
+		c_custkey,
+		c_name,
+		c_acctbal,
+		c_phone,
+		n_name,
+		c_address,
+		c_comment

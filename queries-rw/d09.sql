1451602ebb4d0e39d8474a6fc7b6dbb5ccae9803
hichuang <mhi.chuang@gmail.com>
2012-12-10 00:55:19 -0500

    query9 mv

---
+++ queries-rw/d09.sql
@@ -0,0 +1,26 @@
+-- QUERY 9
+drop table if exists mv_q09;
+create table mv_q09
+	select
+		p_name as mv_pname,
+		n_name as mv_nation,
+		extract(year from o_orderdate) as mv_o_year,
+		sum(l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity) as mv_amount
+	from
+		part,
+		supplier,
+		lineitem,
+		partsupp,
+		orders,
+		nation
+	where
+		s_suppkey = l_suppkey
+		and ps_suppkey = l_suppkey
+		and ps_partkey = l_partkey
+		and p_partkey = l_partkey
+		and o_orderkey = l_orderkey
+		and s_nationkey = n_nationkey
+	group by
+		n_name,
+		mv_o_year,
+		p_name;

80bd45b101901d199a2e38f672c5b568e143436e
hichuang <mhi.chuang@gmail.com>
2012-12-11 01:04:40 -0500

    query14 mv

--- queries-rw/q14.sql
+++ queries-rw/q14.sql
@@ -1,14 +1,10 @@
 -- QUERY 14
 select
-	100.00 * sum(case
-		when p_type like 'PROMO%'
-			then l_extendedprice * (1 - l_discount)
-		else 0
-	end) / sum(l_extendedprice * (1 - l_discount)) as promo_revenue
+	100.00 * sum(case when mv_type like 'PROMO%'
+					then mv_revenue else 0 
+			  	end) / sum(mv_revenue) as promo_revenue 
 from
-	lineitem,
-	part
+	mv_q14
 where
-	l_partkey = p_partkey
-	and l_shipdate >= date '1995-09-01'
-	and l_shipdate < date '1995-09-01' + interval '1' month;
+	mv_shipdate >= date '1995-09-01'
+	and mv_shipdate < date '1995-09-01' + interval '1' month;

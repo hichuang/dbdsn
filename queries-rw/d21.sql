af7162c4c27e7000107c4c3f8758e6ccd5adc9e5
hichuang <mhi.chuang@gmail.com>
2012-12-11 12:08:55 -0500

    query21

--- queries-rw/d21.sql
+++ queries-rw/d21.sql
@@ -1,40 +1,43 @@
 -- QUERY 21
-select
-	s_name,
-	count(*) as numwait
-from
-	supplier,
-	lineitem l1,
-	orders,
-	nation
-where
-	s_suppkey = l1.l_suppkey
-	and o_orderkey = l1.l_orderkey
-	and o_orderstatus = 'F'
-	and l1.l_receiptdate > l1.l_commitdate
-	and exists (
-		select
-			*
-		from
-			lineitem l2
-		where
-			l2.l_orderkey = l1.l_orderkey
-			and l2.l_suppkey <> l1.l_suppkey
-	)
-	and not exists (
-		select
-			*
-		from
-			lineitem l3
-		where
-			l3.l_orderkey = l1.l_orderkey
-			and l3.l_suppkey <> l1.l_suppkey
-			and l3.l_receiptdate > l3.l_commitdate
-	)
-	and s_nationkey = n_nationkey
-	and n_name = 'SAUDI ARABIA'
-group by
-	s_name
-order by
-	numwait desc,
-	s_name;
+drop table if exists mv_q21;
+create table mv_q21
+	select
+		s_name as mv_sname,
+		count(*) as mv_numwait,
+		o_orderstatus as mv_orderstat,
+		n_name as mv_nname
+	from
+		supplier,
+		lineitem l1,
+		orders,
+		nation
+	where
+		s_suppkey = l1.l_suppkey
+		and o_orderkey = l1.l_orderkey
+		and o_orderstatus = 'F'
+		and l1.l_receiptdate > l1.l_commitdate
+		and exists (
+			select
+				*
+			from
+				lineitem l2
+			where
+				l2.l_orderkey = l1.l_orderkey
+				and l2.l_suppkey <> l1.l_suppkey
+		)
+		and not exists (
+			select
+				*
+			from
+				lineitem l3
+			where
+				l3.l_orderkey = l1.l_orderkey
+				and l3.l_suppkey <> l1.l_suppkey
+				and l3.l_receiptdate > l3.l_commitdate
+		)
+		and s_nationkey = n_nationkey
+		and n_name = 'SAUDI ARABIA'
+	group by
+		o_orderstatus,
+		n_name,
+		s_name;
--- queries-rw/q21.sql
+++ queries-rw/q21.sql
@@ -1,40 +1,14 @@
 -- QUERY 21
 select
-	s_name,
-	count(*) as numwait
+	mv_sname as s_name,
+	sum(mv_numwait) as numwait
 from
-	supplier,
-	lineitem l1,
-	orders,
-	nation
+	mv_q21
 where
-	s_suppkey = l1.l_suppkey
-	and o_orderkey = l1.l_orderkey
-	and o_orderstatus = 'F'
-	and l1.l_receiptdate > l1.l_commitdate
-	and exists (
-		select
-			*
-		from
-			lineitem l2
-		where
-			l2.l_orderkey = l1.l_orderkey
-			and l2.l_suppkey <> l1.l_suppkey
-	)
-	and not exists (
-		select
-			*
-		from
-			lineitem l3
-		where
-			l3.l_orderkey = l1.l_orderkey
-			and l3.l_suppkey <> l1.l_suppkey
-			and l3.l_receiptdate > l3.l_commitdate
-	)
-	and s_nationkey = n_nationkey
-	and n_name = 'SAUDI ARABIA'
+	mv_orderstat = 'F'
+	and mv_nname = 'SAUDI ARABIA'
 group by
-	s_name
+	mv_sname
 order by
 	numwait desc,
 	s_name;

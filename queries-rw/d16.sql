702f4c61d37f4fc7ebeb5ec6dff7237ba9c1d989
hichuang <mhi.chuang@gmail.com>
2012-12-11 11:26:12 -0500

    query16 mv

---
+++ queries-rw/d16.sql
@@ -0,0 +1,21 @@
+-- QUERY 16
+drop table if exists mv_q16;
+create table mv_q16
+	select
+		p_brand as mv_brand,
+		p_type as mv_type,
+		p_size as mv_size,
+		s_comment as mv_comment,
+		ps_suppkey as mv_suppkey
+	from
+		partsupp,
+		part
+	where
+		p_partkey = ps_partkey
+		and ps_suppkey = s_suppkey
+	group by
+		p_brand,
+		p_type,
+		p_size,
+		s_comment,
+		ps_suppkey;
--- queries-rw/q16.sql
+++ queries-rw/q16.sql
@@ -1,25 +1,16 @@
 -- QUERY 16
 select
-	p_brand,
-	p_type,
-	p_size,
-	count(distinct ps_suppkey) as supplier_cnt
+	mv_brand as p_brand,
+	mv_type as p_type,
+	mv_size as p_size,
+	count(distinct mv_suppkey) as supplier_cnt
 from
-	partsupp,
-	part
+	mv_q16
 where
-	p_partkey = ps_partkey
-	and p_brand <> 'Brand#45'
-	and p_type not like 'MEDIUM POLISHED%'
-	and p_size in (49, 14, 23, 45, 19, 3, 36, 9)
-	and ps_suppkey not in (
-		select
-			s_suppkey
-		from
-			supplier
-		where
-			s_comment like '%Customer%Complaints%'
-	)
+	mv_brand <> 'Brand#45'
+	and mv_type not like 'MEDIUM POLISHED%'
+	and mv_size in (49, 14, 23, 45, 19, 3, 36, 9)
+	and mv_comment not like '%Customer%Complaints%'
 group by
 	p_brand,
 	p_type,

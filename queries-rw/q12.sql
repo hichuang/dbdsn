88419582f0ad53e5eddb222ca5d2cc764aa4d226
hichuang <mhi.chuang@gmail.com>
2012-12-10 22:55:06 -0500

    query11 mv

--- queries-rw/q12.sql
+++ queries-rw/q12.sql
@@ -1,29 +1,17 @@
 -- QUERY 12
 select
-	l_shipmode,
-	sum(case
-		when o_orderpriority = '1-URGENT'
-			or o_orderpriority = '2-HIGH'
-			then 1
-		else 0
-	end) as high_line_count,
-	sum(case
-		when o_orderpriority <> '1-URGENT'
-			and o_orderpriority <> '2-HIGH'
-			then 1
-		else 0
-	end) as low_line_count
+	mv_shipmode as l_shipmode,
+	case when mv_orderprior in ('1-URGENT', '2-HIGH') 
+		then sum(mv_linecount) end 'high_line_count',
+	case when mv_orderprior not in ('1-URGENT', '2-HIGH') 
+		then sum(mv_linecount) end 'low_line_count'
 from
-	orders,
-	lineitem
+	mv_q12
 where
-	o_orderkey = l_orderkey
-	and l_shipmode in ('MAIL', 'SHIP')
-	and l_commitdate < l_receiptdate
-	and l_shipdate < l_commitdate
-	and l_receiptdate >= date '1994-01-01'
-	and l_receiptdate < date '1994-01-01' + interval '1' year
+	mv_shipmode in ('MAIL', 'SHIP')
+	and mv_recdate >= date '1994-01-01'
+	and mv_recdate < date '1994-01-01' + interval '1' year
 group by
-	l_shipmode
+	mv_shipmode
 order by
 	l_shipmode;

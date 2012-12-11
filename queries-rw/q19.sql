b4a2c1adf3174a7e78006ed8921a094ee5df9730
hichuang <mhi.chuang@gmail.com>
2012-12-11 15:16:29 -0500

    query19

--- queries-rw/q19.sql
+++ queries-rw/q19.sql
@@ -6,7 +6,7 @@ from
 	part
 where
 	(
-		p_partkey = l_partkey
+		l_partkey = p_partkey
 		and p_brand = 'Brand#12'
 		and p_container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG')
 		and l_quantity >= 1 and l_quantity <= 1 + 10
@@ -16,7 +16,7 @@ where
 	)
 	or
 	(
-		p_partkey = l_partkey
+		l_partkey = p_partkey
 		and p_brand = 'Brand#23'
 		and p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK')
 		and l_quantity >= 10 and l_quantity <= 10 + 10
@@ -26,7 +26,7 @@ where
 	)
 	or
 	(
-		p_partkey = l_partkey
+		l_partkey = p_partkey
 		and p_brand = 'Brand#34'
 		and p_container in ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG')
 		and l_quantity >= 20 and l_quantity <= 20 + 10

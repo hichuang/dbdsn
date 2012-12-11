282ff02ac97115a6f90c6c78c7c37e4755ec3555
hichuang <mhi.chuang@gmail.com>
2012-12-11 13:54:28 -0500

    query19

--- queries-rw/q19.sql
+++ queries-rw/q19.sql
@@ -1,13 +1,11 @@
 -- QUERY 19
 select
-	sum(l_extendedprice* (1 - l_discount)) as revenue
+	sum(revenue) as revenue
 from
-	lineitem,
-	part
+	mv_q19
 where
 	(
-		p_partkey = l_partkey
-		and p_brand = 'Brand#12'
+		p_brand = 'Brand#12'
 		and p_container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG')
 		and l_quantity >= 1 and l_quantity <= 1 + 10
 		and p_size between 1 and 5
@@ -16,8 +14,7 @@ where
 	)
 	or
 	(
-		p_partkey = l_partkey
-		and p_brand = 'Brand#23'
+		p_brand = 'Brand#23'
 		and p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK')
 		and l_quantity >= 10 and l_quantity <= 10 + 10
 		and p_size between 1 and 10
@@ -26,8 +23,7 @@ where
 	)
 	or
 	(
-		p_partkey = l_partkey
-		and p_brand = 'Brand#34'
+		p_brand = 'Brand#34'
 		and p_container in ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG')
 		and l_quantity >= 20 and l_quantity <= 20 + 10
 		and p_size between 1 and 15
